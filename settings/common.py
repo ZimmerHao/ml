11d68ebca6bf4d91928792e14aa4d8bbff7c8707
Xi Wang <xi@XIMBP.local>
2019-06-11 23:06:16 +0200

    deployed LB

--- settings/common.py
+++ settings/common.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'oz*l%vik^tjmcg^z^u(&dzleyux257=4my3bistx3*_d3(qjl-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "a2c55cadd8c1f11e9860a0215658c8a6-6be0eabc0a5ca0fa.elb.eu-central-1.amazonaws.com"
+]
 
 APPEND_SLASH = False
 
