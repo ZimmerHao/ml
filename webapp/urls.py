39192848a5e459858c2838a5c8997fdd897cb254
zimmer <zimmer.hao@gmail.com>
2019-06-16 22:17:10 +0800

    show demo log

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path("index/", views.index, name="index")
+    path("index/", views.index, name="webapp.index"),
+    re_path(r'^(?P<pod_name>[^/]+)/$', views.pog_log, name='webapp.pod_log'),
 ]
