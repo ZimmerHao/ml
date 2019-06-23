1eb0736d782e8aac2c9da0e0ba187007a71b18c0
zimmer <zimmer.hao@gmail.com>
2019-06-23 14:20:19 +0800

    add web html

---
+++ frontend/assets/js/main.js
@@ -0,0 +1,32 @@
+document.querySelector('#pod-name-input').onkeyup = function(e) {
+        if (e.keyCode === 13) {  // enter, return
+            document.querySelector('#pod-log-submit').click();
+        }
+};
+
+$("#pod-log-submit").click(function () {
+    var podName = document.querySelector('#pod-name-input').value;
+    $.ajax({
+        type: 'GET',
+        url: '/api/v1/kops/pod_log/',
+        data: {"pod_name": podName},
+        success: function (data) {
+            $("#pod-log").val(data["lines"]);
+        },
+        dataType: "json"
+    });
+});
+
+$("#yaml-url-submit").click(function () {
+    var yamlURL = document.querySelector('#yaml-url-input').value;
+    alert(yamlURL);
+    $.ajax({
+        type: 'POST',
+        url: '/api/v1/kops/apply_yaml/',
+        data: {"yaml_url": yamlURL},
+        success: function (data) {
+            alert("create success");
+        },
+        dataType: "json"
+    });
+});
