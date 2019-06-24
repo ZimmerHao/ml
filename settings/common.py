0727fbec7107ff9230401bb060d418c577357625
Xi Wang <xi@XIMBP.local>
2019-06-24 09:32:45 +0200

    added parser and change inputtext to textarea

--- settings/common.py
+++ settings/common.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oz*l%vik^tjmcg^z^u(&dzleyux257=4my3bistx3*_d3(qjl-'
+SECRET_KEY = "oz*l%vik^tjmcg^z^u(&dzleyux257=4my3bistx3*_d3(qjl-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,102 +32,105 @@ APPEND_SLASH = False
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'django_extensions',
-    'channels',
-    'apps.kauth',
-    'apps.user',
-    'apps.chat',
-    'apps.kops',
-    'web',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "django_extensions",
+    "channels",
+    "apps.kauth",
+    "apps.user",
+    "apps.chat",
+    "apps.kops",
+    "web",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core.middleware.BusinessExceptionMiddleware'
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "core.middleware.BusinessExceptionMiddleware",
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = ('*',)
+CORS_ORIGIN_WHITELIST = ("*",)
 CORS_ALLOW_HEADERS = (
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
 )
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+    "DEFAULT_PARSER_CLASSES": (
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
     ),
 }
 
-AUTH_USER_MODEL = 'user.User'
+AUTH_USER_MODEL = "user.User"
 
-AUTHENTICATION_BACKENDS = ['apps.user.backends.UserBackend',
-                           'apps.user.backends.OAuth2Backend']
+AUTHENTICATION_BACKENDS = [
+    "apps.user.backends.UserBackend",
+    "apps.user.backends.OAuth2Backend",
+]
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = "urls"
 
-FRONTEND_DIR = os.path.join(BASE_DIR, 'frontend')
+FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(FRONTEND_DIR, "assets/templates")],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(FRONTEND_DIR, "assets/templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
         },
-    },
+    }
 ]
 
-WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = "wsgi.application"
 
-ASGI_APPLICATION = 'routing.application'
+ASGI_APPLICATION = "routing.application"
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('redis-master.default.svc.cluster.local', 6379)],
-        },
-    },
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {"hosts": [("redis-master.default.svc.cluster.local", 6379)]},
+    }
 }
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ml',
-        'USER': 'postgres',
-        'HOST': '192.168.50.20',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ml",
+        "USER": "postgres",
+        "HOST": "192.168.50.20",
+        "PORT": "5432",
     }
 }
 
@@ -136,25 +139,19 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -165,52 +162,40 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/frontend/'
+STATIC_URL = "/frontend/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "frontend"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend")]
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
         },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
+        "simple": {"format": "%(levelname)s %(message)s"},
+    },
+    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "logs/django_log.log",
         },
     },
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse',
-        }
-    },
-    'handlers': {
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': 'logs/django_log.log',
+    "loggers": {
+        "django": {"handlers": ["console"], "level": "DEBUG", "propagate": True},
+        "django.request": {
+            "handlers": ["file", "console"],
+            "level": "INFO",
+            "propagate": False,
         },
     },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django.request': {
-            'handlers': ['file', 'console'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-    }
 }
 
 REDIS_HOST = "192.168.90.10"
@@ -218,47 +203,40 @@ REDIS_PORT = 6379
 
 # Celery Configuration
 # CELERY_BROKER_URL = 'redis://:password@hostname:port/db_number'
-CELERY_BROKER_URL = 'amqp://admin:admin@127.0.0.1:5670//'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/8'
-CELERY_TASK_SERIALIZER = 'msgpack'
-CELERY_RESULT_SERIALIZER = 'json'
+CELERY_BROKER_URL = "amqp://admin:admin@127.0.0.1:5670//"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/8"
+CELERY_TASK_SERIALIZER = "msgpack"
+CELERY_RESULT_SERIALIZER = "json"
 CELERY_RESULT_EXPIRES = 60 * 60 * 24
-CELERY_ACCEPT_CONTENT = ['json', 'msgpack']
-CELERY_TIMEZONE = 'Asia/Shanghai'
+CELERY_ACCEPT_CONTENT = ["json", "msgpack"]
+CELERY_TIMEZONE = "Asia/Shanghai"
 CELERY_ENABLE_UTC = True
 CELERY_TASK_TIME_LIMIT = 60 * 6
 
-CELERY_IMPORTS = (
-    'apps.tasks',
-)
+CELERY_IMPORTS = ("apps.tasks",)
 
 CELERY_TASK_QUEUES = (
-    Queue('default', Exchange('tasks', 'topic'), routing_key='task.#'),
-    Queue('api', Exchange('api', 'topic'), routing_key='api.#'),
+    Queue("default", Exchange("tasks", "topic"), routing_key="task.#"),
+    Queue("api", Exchange("api", "topic"), routing_key="api.#"),
 )
 
-CELERY_TASK_DEFAULT_QUEUE = 'default'
-CELERY_TASK_DEFAULT_EXCHANGE = 'tasks'
-CELERY_TASK_DEFAULT_EXCHANGE_TYPE = 'topic'
-CELERY_TASK_DEFAULT_ROUTING_KEY = 'task.default'
+CELERY_TASK_DEFAULT_QUEUE = "default"
+CELERY_TASK_DEFAULT_EXCHANGE = "tasks"
+CELERY_TASK_DEFAULT_EXCHANGE_TYPE = "topic"
+CELERY_TASK_DEFAULT_ROUTING_KEY = "task.default"
 
-CELERY_TASK_ROUTES = {
-    'apps.tasks.add': {
-        'queue': 'api',
-        'routing_key': 'api.add'
-    }
-}
+CELERY_TASK_ROUTES = {"apps.tasks.add": {"queue": "api", "routing_key": "api.add"}}
 
-CELERY_BEAT_SCHEDULER = 'celery.beat:PersistentScheduler'
+CELERY_BEAT_SCHEDULER = "celery.beat:PersistentScheduler"
 
 CELERY_BEAT_SCHEDULE = {
-    'add-every-30-seconds': {
-        'task': 'apps.tasks.add',
-        'schedule': 30.0,
-        'args': (16, 16)
-    },
+    "add-every-30-seconds": {
+        "task": "apps.tasks.add",
+        "schedule": 30.0,
+        "args": (16, 16),
+    }
 }
 
 USE_CELERY = False
 
-SILENCED_SYSTEM_CHECKS = ['auth.W004']
+SILENCED_SYSTEM_CHECKS = ["auth.W004"]
